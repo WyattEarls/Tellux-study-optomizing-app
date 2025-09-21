164508914efa381ba915bdd70bd00d48c4378654
Wyatt Earls <wyatt.earls@my.utsa.edu>
2025-09-21 13:45:07 -0500

    Hello Tellux

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello, Tellux!</h1>
+      <p1>This is you're personal study optimizing tool that will create study blocks for you to effectively manage your courses.</p1>
+      </div>
+      
+  )
+}
+
+export default App
