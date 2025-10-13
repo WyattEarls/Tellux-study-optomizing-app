3b718fd1beb89030694a1df9c6eeb7b2f8c25763
Wyatt Earls <wyatt.earls@my.utsa.edu>
2025-10-13 10:17:42 -0500

    Implement sign up feature and refactored routes

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 # importing the neccessary files as API's
 from flask import Flask
+from flask_cors import CORS # pyright: ignore[reportMissingModuleSource]
 from api.canvas_routes import canvas_bp
 from api.user_routes import user_bp
 from api.schedule_routes import schedule_bp
 
 app = Flask(__name__)
+CORS(app)
 
 # Registering bp's 
 app.register_blueprint(canvas_bp, url_prefix = '/api/canvas')
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleRegister = async (e) => {
+    e.preventDefault()
+    const res =  await fetch('http://127.0.0.1:5000/register', {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({email, password}),
+    })
+    const data = await res.json()
+    setMessage(data.message || data.error)
+  }
 
   return (
-    <div>
-      <h1>Hello, Tellux!</h1>
-      <p1>This is you're personal study optimizing tool that will create study blocks for you to effectively manage your courses.</p1>
-      </div>
-      
+    <div className="app">
+      <h1>Register for Tellux!</h1>
+      <form onSubmit={handleRegister}>
+        <input 
+          type = 'email'
+          placeholder='Enter your email'
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}
+          required
+        />
+        <input 
+          type='password'
+          placeholder='Enter your password'
+          value={password}
+          onChange= {(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+        {message && <p>{message}</p>}
+      </div> 
   )
 }
 
